c59ed44a0c495c85872b47b30ce54424c3150caa
sildem <98412151+sildem@users.noreply.github.com>
2022-04-01 10:00:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git jerrykuku_argon https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
+echo 'src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages.git' >>feeds.conf.default
 
 # Add openclash
 git clone https://github.com/vernesong/OpenClash.git
